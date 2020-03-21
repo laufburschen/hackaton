06ede84a73c07d8dc058b2f8d271643f7ed91e9e
Philipp Meißner <pm@zeit.dev>
2020-03-21 14:36:18 +0100

    init project structure (#1)
    * init project structure
    
    * initial angular app
    
    * gitignore
    
    * used headless browser for testing
    
    Co-authored-by: Elias Ehmann <eliaz.develops@gmail.com>
    Co-authored-by: Büscher <f.buescher@shopware.com>
    Co-authored-by: gerhard <gerhard@gerhard.com>

---
+++ ClientApp/karma.conf.js
@@ -0,0 +1,38 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    browsers: ['ChromiumNoSandbox'],
+    customLaunchers: {
+      ChromiumNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions']
+      },
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/ClientApp'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
