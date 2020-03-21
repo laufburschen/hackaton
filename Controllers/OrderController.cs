fbcad7dcd170db7fa5c2142bb8c6ea158430410e
gerhard <gerhard@gerhard.com>
2020-03-21 23:59:52 +0100

    list and get orders via rest api

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -10,10 +10,8 @@ using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
-    public class CreateOrderDto
+    public class OrderCreateDto
     {
-        public string id {get; set;}
-
         public string product {get; set;}
 
         public int items  {get; set;}
@@ -22,6 +20,26 @@ namespace WebApplication.Controllers
 
         public string comment  {get; set;}  
     }
+
+    public class OrderGetDto : OrderCreateDto
+    {
+        public string id {get; set;}
+    }
+
+    public static class OrderExtensions
+    {
+        public static OrderGetDto CreateGetDto(this Order order)
+        {
+            return new  OrderGetDto {
+                id = order.id,
+                product = order.product,
+                comment = order.comment,
+                items = order.items,
+                maximum_price_per_item = order.maximum_price_per_item
+            };
+        }
+    }
+
 
     [ApiController]
     [Route("[controller]")]
@@ -45,10 +63,8 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost]
-        public async Task<string> Create([FromBody][Required]CreateOrderDto createOrderArgs)
+        public async Task<string> Create([FromBody][Required]OrderCreateDto createOrderArgs)
         {
-            await Task.CompletedTask;
-
             var id = $"order_{Guid.NewGuid():N}";
             _context.Orders.Add(
                 new Order {
@@ -60,7 +76,25 @@ namespace WebApplication.Controllers
                 });
             await _context.SaveChangesAsync();
 
-            return "coming_soon";
+            return id;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<OrderGetDto>> Get()
+        {
+            return _context.Orders.ToList().Select(_ => _.CreateGetDto()).ToArray();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<IEnumerable<OrderGetDto>> Get(string id)
+        {
+            var foundOrder = _context.Orders.FirstOrDefault(_ => _.id == id);
+            if (foundOrder != null)
+            {
+                return new [] { foundOrder.CreateGetDto() };
+            }
+            return BadRequest();
         }
     }
 }
