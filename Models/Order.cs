afa522b57b2487b57e41477855bd76c946bc6216
gerhard <gerhard@gerhard.com>
2020-03-21 14:17:58 +0100

    first model

---
+++ Models/Order.cs
@@ -0,0 +1,7 @@
+namespace WebApplication.Models
+{
+    public class Order
+    {
+        public string issuer_id {get; set;}
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+namespace WebApplication.Models
+{    
+    public class Person 
+    {
+        public string Id {get; set; }
+
+        public string FirstName { get; set;}
+
+        public int Salary { get; set;    }
+    }
+}
