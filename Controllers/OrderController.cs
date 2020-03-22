885f3a22a96f4a54a94487c7494b5a4718b52e85
gerhard <gerhard@gerhard.com>
2020-03-22 07:55:23 +0100

    rename route to lowercase

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -42,17 +42,11 @@ namespace WebApplication.Controllers
 
 
     [ApiController]
-    [Route("[controller]")]
+    [Route("order")]
     public class OrderController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<OrderController> _logger;
         private readonly MariaContext _context;
-
 
         public OrderController(
             MariaContext context,
